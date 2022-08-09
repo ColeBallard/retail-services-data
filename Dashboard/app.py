8c524056f0420c84685251c3e8c249984adfc12e
Cole Ballard <cole.ballard97@gmail.com>
2022-08-09 15:51:38 -0500

    configured env variables

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -5,10 +5,25 @@ import plotly.graph_objects as go
 import pymssql
 from dash.dependencies import Input, Output
 import numpy as np
+import os
 
-from config import database, user, password, dateTable, macroTable, NAICS_NAPCS, NAICSTable, NAPCSTable, server
+if os.getenv("app") != "prod":
+    from config import database, user, password, dateTable, macroTable, NAICS_NAPCS, NAICSTable, NAPCSTable, server
 
-app = Dash(__name__)
+else:
+    database = os.getenv("Password")
+    dateTable = os.getenv("dateTable")
+    macroTable = os.getenv("macroTable")
+    NAICS_NAPCS = os.getenv("NAICS_NAPCS")
+    NAICSTable = os.getenv("NAICSTable")
+    NAPCSTable = os.getenv("NAPCSTable")
+    user = os.getenv("user")
+    password = os.getenv("password")
+    server = os.getenv("server")
+    
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 def createBarGraph():
 
@@ -34,8 +49,19 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='example-graph',
         figure=createBarGraph()
-    )
+    ),
+
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
 ])
+
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
