1ff8e4a475e23ab96dcb18df1fcd24c8412c4ed2
Cole Ballard <cole.ballard97@gmail.com>
2022-08-10 13:55:32 -0500

    clean up code and add comments

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -7,9 +7,11 @@ import sqlalchemy as sa
 import flask
 import os
 
+# get environment variables from config if production environment
 if os.getenv("app_environment") != "production":
     from config import database, user, password, dateTable, macroTable, NAICS_NAPCS, NAICSTable, NAPCSTable, serverName
 
+# get environment variables from heroku if development environment
 else:
     database = os.getenv("database")
     dateTable = os.getenv("dateTable")
@@ -20,46 +22,52 @@ else:
     user = os.getenv("user")
     password = os.getenv("password")
     serverName = os.getenv("serverName")
-    
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__)
 
-app = Dash(__name__, external_stylesheets=external_stylesheets, server=server)
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'], server = server)
 
 def createBarGraph():
     
+    # set parameters for connection string
     params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
-                                     "SERVER="+serverName+";"
-                                     "DATABASE="+database+";"
-                                     "UID="+user+";"
-                                     "PWD="+password+";")
+                                     "SERVER=" + serverName + ";"
+                                     "DATABASE=" + database + ";"
+                                     "UID=" + user + ";"
+                                     "PWD=" + password + ";")
 
     engine = sa.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(params))
 
+    # example query
     query = f"SELECT * FROM {macroTable}"
 
-    con = engine.connect()
+    conn = engine.connect()
     
-    df = pd.read_sql(query,con)
+    # create dataframe from query
+    df = pd.read_sql(query, conn)
 
-    fig = px.bar(df, x='MacroID', y='CPI', title='MacroID by CPI',
-        labels={'MacroID': 'Macro ID', 'CPI': 'CPI'})
+    # example graph
+    fig = px.bar(df, x='MacroID', y='CPI', title='MacroID by CPI', labels = {'MacroID': 'Macro ID', 'CPI': 'CPI'})
 
     return fig
 
 app.layout = html.Div(children=[
+
+    # title
     html.H1(children='Retail Service Data'),
 
+    # subtitle
     html.Div(children='''
         An overview of retail data over time in the United States. 
     '''),
 
+    # example graph
     dcc.Graph(
         id='example-graph',
         figure=createBarGraph()
     ),
 
+    # example dropdown
     dcc.Dropdown(['LA', 'NYC', 'MTL'],
         'LA',
         id='dropdown'
@@ -67,6 +75,7 @@ app.layout = html.Div(children=[
     html.Div(id='display-value')
 ])
 
+# example callback for dropdown
 @app.callback(Output('display-value', 'children'),
                 [Input('dropdown', 'value')])
 def display_value(value):
@@ -75,4 +84,4 @@ def display_value(value):
 if __name__ == '__main__':
     app.run_server(debug=True)
 
-# http://127.0.0.1:8050/
+# local development server runs on http://127.0.0.1:8050/
