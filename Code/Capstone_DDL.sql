f30afe7101fa61033dd99b1416ade14e6655ba00
Gavan VanOver <gavanover27@gmail.com>
2022-08-15 11:34:08 -0400

    Updated DDL

--- Code/Capstone_DDL.sql
+++ Code/Capstone_DDL.sql
@@ -1,26 +1,29 @@
-drop database if exists [capstone-group6-database];
+drop table if exists SalesTable;
 GO
-
-create database [capstone-group6-database];
+drop table if exists NAICS_NAPCS;
 GO
-
-use [capstone-group6-database];
+drop table if exists MacroTable;
+GO
+drop table if exists NAPCSTable;
+GO
+drop table if exists NAICSTable;
+GO
+drop table if exists DateTable;
 GO
 
 CREATE TABLE DateTable (
     DateID int PRIMARY KEY IDENTITY(1,1),
-    DateYear INT NOT NULL,
-    DateMonth INT, 
-    DateDay int
+    [Year] INT,
+    [Month] INT, 
 );
 
 CREATE TABLE MacroTable(
     MacroID INT PRIMARY KEY IDENTITY(1,1),
     DateID INT NOT NULL,
-    CPI DECIMAL,
-    RPI DECIMAL,
-    USTRADE DECIMAL,
-    USWTRADE DECIMAL,
+    CPI DECIMAL(18,10),
+    RPI DECIMAL(18,10),
+    USTRADE DECIMAL(18,10),
+    USWTRADE DECIMAL(18,10),
     CONSTRAINT fk_MacroTable_DateID
         FOREIGN KEY (DateID)
         REFERENCES DateTable(DateID)
@@ -28,35 +31,35 @@ CREATE TABLE MacroTable(
 
 CREATE TABLE NAICSTable (
     NAICSID INT PRIMARY KEY IDENTITY(1,1),
-    NAICSname VARCHAR(50),
-    NAICSCode INT
+    [2017 NAICS code (NAICS2017)] BIGINT,
+    [Meaning of NAICS code (NAICS2017_LABEL)] VARCHAR(200)
 );
 
 CREATE TABLE SalesTable (
     SalesID INT PRIMARY KEY IDENTITY(1,1),
     DateID INT,
     NAICSID INT,
-    AdjustedSales DECIMAL,
-    Sales DECIMAL,
+    [Unadjusted Sales] DECIMAL (18,4),
+    [Adjusted Sales] DECIMAL (18,4),
+    CONSTRAINT fk_SalesTable_NAICSID
+        FOREIGN KEY (NAICSID)
+        REFERENCES NAICSTable(NAICSID), 
     CONSTRAINT fk_SalesTable_DateID
         FOREIGN KEY (DateID)
         REFERENCES DateTable(DateID),
-    CONSTRAINT fk_SalesTable_NAICSID
-        FOREIGN KEY (NAICSID)
-        REFERENCES NAICSTable(NAICSID)
 );
 
 CREATE TABLE NAPCSTable (
     NAPCSID INT PRIMARY KEY IDENTITY(1,1),
-    NAPCSCode INT,
-    NAPCSName VARCHAR(50)
+    [2017 NAPCS collection code (NAPCS2017)] BIGINT,
+    [Meaning of NAPCS collection code (NAPCS2017_LABEL)] VARCHAR(250)
 );
 
-CREATE TABLE NAISC_NAPCS (
+CREATE TABLE NAICS_NAPCS (
     NAICSID INT,
     NAPCSID INT,
-    NumberofEstablishments INT,
-    SalesValueofshipmentsorRevenue INT,
+    [Number of establishments (ESTAB)] INT,
+    [Sales, value of shipments, or revenue of NAPCS collection code ($1,000) (NAPCSDOL)] INT,
     PRIMARY KEY (NAICSID, NAPCSID),
     CONSTRAINT fk_NAISC_NAPCS_NAICSID
         FOREIGN KEY (NAICSID)
@@ -64,4 +67,4 @@ CREATE TABLE NAISC_NAPCS (
     CONSTRAINT fk_NAISC_NAPCS_NAPCSID
         FOREIGN KEY (NAPCSID)
         REFERENCES NAPCSTable(NAPCSID)
-)
+);
